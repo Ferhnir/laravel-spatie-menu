cbc1d6ca63fb7b55ef4692c61747ed2580742455
makszdunski <maksymilian.zdunski@huddled.media>
2022-07-20 15:13:21 +0200

    Created service to render menu

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# Soon I will add more
+# Menu Generator for spatie/laravel-menu
+This generator provides a support for spatie/laravel-menu with php8
+
+# Example:
+### Links:
+```php
+#serviceProvider:
+
+view()->composer(['your.blade'], function () {
+    app('your-instance-name')->add(
+        LaravelSpatieMenuService::createLink(
+            route: route('buildings.index'),
+            title: 'Buildings',
+            icon: '<i class="menu-icon fas fa-file-alt fa-3x"></i>'
+        )
+    );
+});
+```
+
+### Submenus:
+```php
+view()->composer(['your.blade'], function () {
+
+    $uniqueSubmenuID = 'ExampleID';
+
+    //CREATE SUBMENU MAIN LINK
+    $linkNew = LaravelSpatieMenuService::createLink(
+        route: '#',
+        title: '<span class="menu-label">ExampleLink</span>',
+        icon: '<i class="menu-icon far fa-sitemap fa-3x"></i>',
+        uniqueSubmenuID: $uniqueSubmenuID
+    );
+
+    //CREATE SUBMENU OBJECT
+    //Create submenuList for submenuObj
+    $submenuList = LaravelSpatieMenuService::buildSubmenuList(
+        submenuLinksArray: [
+            [
+                'title' => 'Show All',
+                'icon' => '<i class="far fa-eye"></i>',
+                'url' => route('example.index'),
+                'rule' => ['browse-example']
+            ], [
+                'title' => 'Add Company',
+                'icon' => '<i class="far fa-plus"></i>',
+                'url' => route('example.create'),
+                'rule' => ['add-example']
+            ], [
+                'title' => 'Archive',
+                'icon' => '<i class="far fa-cabinet-filing"></i>',
+                'url' => route('example.archive'),
+                'rule' => ['delete-example']
+            ],
+            ...
+        ]
+    );
+
+    //Create submenuObj
+    $submenuObj = LaravelSpatieMenuService::newMenu(
+        uniqueSubmenuID: $uniqueSubmenuID,
+        wrapperTag: 'div',
+        parentTag: false,
+        activeClassOnLink: true
+    );
+
+    //Attach submenuObj to menu instance
+    app('your-instance-name')->submenu(
+        $linkNew,
+        $submenuObj->add($submenuList)
+    );
+
+});
+```
+
+### Blade 'your.blade'
+```php 
+{!! app('your-instance-name') !!}
+```
--- src/LaravelSpatieMenuService.php
+++ src/LaravelSpatieMenuService.php
@@ -2,10 +2,133 @@
 
 namespace Ferhnir\LaravelSpatieMenu;
 
-class LaravelSpatieMenuService
+use Spatie\Menu\Laravel\Menu;
+use Spatie\Menu\Link;
+
+class LaravelSpatieMenuService extends Menu
 {
-    public function __construct()
+    /**
+     * @param string|null $class
+     * @param string|null $parentClass
+     * @param array $attrs
+     * @param string|null $wrapperTag
+     * @param bool $parentTag
+     * @param bool $activeClassOnLink
+     * @return Menu
+     */
+    public static function newMenu(
+        string $class = null,
+        string $parentClass = null,
+        array $attrs = [],
+        string $uniqueSubmenuID = null,
+        string $wrapperTag = null,
+        bool $parentTag = true,
+        bool $activeClassOnLink = false
+    ) : Menu
     {
-        dd('hello World!');
+        $newMenu = Menu::new();
+
+        if ($uniqueSubmenuID) {
+            $class .= ' collapse';
+            $parentClass = 'nav-item';
+            $attrs = [
+                'id' => $uniqueSubmenuID
+            ];
+        }
+
+        if ($class)
+            $newMenu->addClass($class);
+
+        if ($parentClass)
+            $newMenu->addParentClass($parentClass);
+
+        if ($attrs)
+            $newMenu->setAttributes($attrs);
+
+        if ($wrapperTag)
+            $newMenu->setWrapperTag($wrapperTag);
+
+        if (!$parentTag)
+            $newMenu->withoutParentTag(false);
+
+        if ($activeClassOnLink)
+            $newMenu->setActiveClassOnLink();
+
+        return $newMenu;
+    }
+
+    /**
+     * @param string $route
+     * @param string $title
+     * @param string $icon
+     * @param string $class
+     * @param string $parentClass
+     * @param array $attrs
+     * @param string|null $uniqueSubmenuID
+     * @return Link
+     */
+    public static function createLink(
+        string $route,
+        string $title,
+        string $icon,
+        string $class = 'nav-link',
+        string $parentClass = 'nav-item',
+        array $attrs = [],
+        string $uniqueSubmenuID = null
+    ) : Link
+    {
+        $attributes = [...$attrs];
+
+        $submenuAttrs = [
+            'data-toggle' => 'collapse',
+            'aria-expanded' => 'false',
+            'aria-controls' => $uniqueSubmenuID,
+            'href' => '#' . $uniqueSubmenuID
+        ];
+
+        if ($uniqueSubmenuID)
+            $attributes = [...$attributes ,...$submenuAttrs];
+
+
+        return Link::to($route, $icon . $title)
+            ->addClass($class)
+            ->addParentClass($parentClass)
+            ->setAttributes($attributes);
+    }
+
+    /**
+     * @param array $submenuLinksArray
+     * @return Menu
+     */
+    public static function buildSubmenuList(
+        array $submenuLinksArray = []
+    ) : Menu
+    {
+        $submenuList = self::newMenu(
+            class: 'nav flex-column sub-menu',
+            attrs: [
+                'style' => 'list-style-type: none;'
+            ]
+        );
+
+        foreach ($submenuLinksArray as $key => $submenuLink) {
+            if (
+                !isset($submenuLink['rule']) ||
+                (!empty($submenuLink['rule']) && auth()->user()->can($submenuLink['rule']))
+            ) {
+                $submenuList->add(
+                    self::createLink(
+                        route: $submenuLink['url'],
+                        title: $submenuLink['title'],
+                        icon: $submenuLink['icon'],
+                        class: $submenuLink['class'] ?? 'nav-link',
+                        parentClass: $key == 0 ? $submenuLink['parentClass'] ?? 'nav-item' : 'nav-item',
+                        attrs: $submenuLink['attrs'] ?? []
+                    )
+                );
+            }
+        }
+
+        return $submenuList;
     }
 }
